a9fa0ff978ba06c7be5228c30c1f94dc2672617d
vfagesgo <vfagesgo@gmail.com>
2026-09-02 16:08:35 +0200

    MCP

--- linux/install.sh
+++ linux/install.sh
@@ -809,6 +809,7 @@ if [ -f "/home/denodo/Denodo MCP Server.zip" ]; then
   log_step "Installer archive already downloaded, skipping (remove /home/denodo/Denodo MCP Server.zip to force a re-download)"
 else
   log_step "Download Denodo MCP Installer"
+  cd $TARGET_DIR/denodo-support-utils/bin/
   ./denodo-support -t installer -n 'Denodo MCP Server' -d /home/denodo -u $DENODO_SUPPORT_CI -s $DENODO_SUPPORT_SECRET
 fi
 
